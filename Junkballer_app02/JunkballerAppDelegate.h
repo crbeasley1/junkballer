6eade238bed9465ae4abc4b6a70753e9e500ed67
Chip R. Beasley <crbeasley1@gmail.com>
2014-06-04 22:06:23 -0500

    Initial Commit

---
+++ Junkballer_app02/JunkballerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JunkballerAppDelegate.h
+//  Junkballer_app02
+//
+//  Created by CR Beasley on 6/4/14.
+//  Copyright (c) 2014 TennisIQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JunkballerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Junkballer_app02/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Junkballer_app02
+//
+//  Created by CR Beasley on 6/4/14.
+//  Copyright (c) 2014 TennisIQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JunkballerAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JunkballerAppDelegate class]));
+    }
+}
